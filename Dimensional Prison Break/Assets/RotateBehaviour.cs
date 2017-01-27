f1c0b7b6e12b375f926e35ac8962ddb9738bfa73
auusi9 <ausi_dalmau@hotmail.com>
2017-01-27 15:35:14 +0100

    Player moving with WASD and rotatin Up looking at the mouse, very strange

---
+++ Dimensional Prison Break/Assets/RotateBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateBehaviour : MonoBehaviour {
+
+    Vector3 movement;
+    Animator anim;
+    Rigidbody hipRigidbody;
+    int floorMask;
+    float camRayLength = 100f;
+    public GameObject spine;
+
+    void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        //hipRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Turning();
+
+    }
+
+
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - spine.transform.position;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            Debug.Log("asaa");
+            spine.transform.rotation = newRotation;
+        }
+    }
+}
---
+++ Dimensional Prison Break/Assets/Scripts/Nueva carpeta/FollowPlayer.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    float last_angle = 0.0f;
+    public int x = 10;
+    public int y = 10;
+    public int z = 10;
+    Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.position = new Vector3(player.transform.position.x + x, player.transform.position.y + y, player.transform.position.z + z);
+        transform.LookAt(player.transform);
+        offset = player.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = player.transform.position - offset;
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            last_angle += 45.0f;
+            transform.RotateAround(player.transform.position, Vector3.up, 45.0f);
+            transform.LookAt(player.transform);
+            offset = player.transform.position - transform.position;
+        }
+        else if (Input.GetButtonDown("Fire3"))
+        {
+            last_angle -= 45.0f;
+            transform.RotateAround(player.transform.position, Vector3.up, -45.0f);
+            transform.LookAt(player.transform);
+            offset = player.transform.position - transform.position;
+        }
+    }
+}
